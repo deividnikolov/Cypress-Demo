0757a244e8918ebcc7954692515e2e3e51654dc9
deividnikolov <davidnikolov990@gmail.com>
2021-08-11 05:02:34 +0300

    formatting

--- cypress/integration/tests/LoginTests.spec.js
+++ cypress/integration/tests/LoginTests.spec.js
@@ -7,7 +7,8 @@ describe("Login Tests Sauce Demo Labs", () => {
     cy.viewport(1280, 720);
     cy.log("Opening the website");
     cy.visit(Cypress.env("url"));
-    cy.url().should("include", "saucedemo");
+    cy.url()
+    .should("include", "saucedemo");
   });
 
   it("should be able to login with valid credentials ", () => {
@@ -16,7 +17,8 @@ describe("Login Tests Sauce Demo Labs", () => {
   });
 
   it("should not be able to login with invalid credentials", () => {
-    loginPage.login("wrongUsername ", "wrongPassword");
+    loginPage.
+    login("wrongUsername ", "wrongPassword");
     loginPage.elements
       .errorMessage()
       .should(
@@ -24,14 +26,18 @@ describe("Login Tests Sauce Demo Labs", () => {
         "Epic sadface: Username and password do not match any user in this service"
       );
   });
+  
   it("should logout", () => {
-    loginPage.login("standard_user", "secret_sauce");
+    loginPage.
+    login("standard_user", "secret_sauce");
     loginPage.clickLogoutButton();
-    cy.get(".login_logo").should("be.visible");
+    cy.get(".login_logo")
+    .should("be.visible");
   });
 
   it("should not be able to login with locked_out_user", () => {
-    loginPage.login("locked_out_user", "secret_sauce");
+    loginPage.
+    login("locked_out_user", "secret_sauce");
     loginPage.elements
       .errorMessage()
       .should(
@@ -39,28 +45,38 @@ describe("Login Tests Sauce Demo Labs", () => {
         "Epic sadface: Sorry, this user has been locked out."
       );
   });
+
   it("should login with performance glitch user", () => {
-    loginPage.login("performance_glitch_user", "secret_sauce");
+    loginPage.
+    login("performance_glitch_user", "secret_sauce");
     inventoryPage.verifyTitleExist();
   });
+
   it("should have the correct image color", () => {
-    loginPage.login("problem_user", "secret_sauce");
-    cy.get("#item_4_img_link > .inventory_item_img").should(
+    loginPage.
+    login("problem_user", "secret_sauce");
+    cy.get("#item_4_img_link > .inventory_item_img")
+    .should(
       "have.css",
       "color",
       "rgb(0, 0, 238)"
     );
   });
+
   it("The product image should have the correct width", () => {
-    loginPage.login("standard_user", "secret_sauce");
+    loginPage.
+    login("standard_user", "secret_sauce");
     cy.get("#item_4_img_link > .inventory_item_img")
       .should("have.css", "width")
       .and("eq", "186.75px");
   });
+
   it("The product image should have the correct height", () => {
-    loginPage.login("standard_user", "secret_sauce");
+    loginPage.
+    login("standard_user", "secret_sauce");
     cy.get("#item_4_img_link > .inventory_item_img")
       .should("have.css", "height")
       .and("eq", "237.4375px");
+
   });
 });
